4fdff6036411786e5ee8f32cd4f099a35ab029ae
vanshkumar740 <vanshdishodia@gmail.com>
2025-07-04 21:28:35 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+document.querySelectorAll('.btn-primary').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Redirecting to blog post...');
+    });
+});
